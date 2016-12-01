22e0050513da6f6cb5b542e39b7865d13059acfb
Asim Qasimzade <asim.qasimzade@gmail.com>
2016-12-01 17:16:15 +0300

    Create Toolbar with 3 fixed tabs connected to ViewPager with 3 fragments

---
+++ app/src/main/java/com/asimqasimzade/android/neatwallpapers/FiveFragment.java
@@ -0,0 +1,33 @@
+package com.asimqasimzade.android.neatwallpapers;
+
+/**
+ * Created by Asim on 12/1/2016.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FiveFragment extends Fragment{
+
+    public FiveFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_five, container, false);
+    }
+
+}
