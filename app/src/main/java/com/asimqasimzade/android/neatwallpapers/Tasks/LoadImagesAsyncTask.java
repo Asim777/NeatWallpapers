19886b36f43409e38aa49620a8cae8050e5035c6
Asim Qasimzade <asim.qasimzade@gmail.com>
2016-12-16 14:47:11 +0300

    Change LoadImageAsycnTask to load not just first page, but all pages

--- app/src/main/java/com/asimqasimzade/android/neatwallpapers/Tasks/LoadImagesAsyncTask.java
+++ app/src/main/java/com/asimqasimzade/android/neatwallpapers/Tasks/LoadImagesAsyncTask.java
@@ -12,13 +12,13 @@ import android.widget.Toast;
 
 import com.asimqasimzade.android.neatwallpapers.Adapters.ImagesGridViewAdapter;
 import com.asimqasimzade.android.neatwallpapers.Data.GridItem;
-import com.asimqasimzade.android.neatwallpapers.MainActivity;
 import com.asimqasimzade.android.neatwallpapers.PopularFragment;
 import com.asimqasimzade.android.neatwallpapers.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -42,9 +42,10 @@ public class LoadImagesAsyncTask extends AsyncTask<String, Void, Integer> {
     private Context mContext;
     private View mRootView;
     private String mUrl;
+    private int numberOfPages = 1;
 
 
-    public LoadImagesAsyncTask(Context context, View rootView, String url){
+    public LoadImagesAsyncTask(Context context, View rootView, String url) {
         mContext = context;
         mRootView = rootView;
         mUrl = url;
@@ -64,29 +65,36 @@ public class LoadImagesAsyncTask extends AsyncTask<String, Void, Integer> {
     protected Integer doInBackground(String... params) {
         Integer result = 0;
 
-        try{
-            feed_url = new URL(mUrl);
-        } catch (MalformedURLException e){
-            e.printStackTrace();
-        }
 
-        try {
-            //Create Apache HttpClient
-            urlConnection = (HttpURLConnection) feed_url.openConnection();
-            int statusCode =  urlConnection.getResponseCode();
+        //Loop through pages
+        for (int i = 0; i < numberOfPages; i++) {
+            try {
+                mUrl = mUrl + "&page=" + (i + 1);
+                feed_url = new URL(mUrl);
+                //Create Apache HttpClient
+                urlConnection = (HttpURLConnection) feed_url.openConnection();
+                int statusCode = urlConnection.getResponseCode();
 
-            //200 represent status is OK
-            if(statusCode == 200){
-                String response = streamToString(urlConnection.getInputStream());
-                parseResult(response);
-                result = 1; //Successful
-            } else {
-                result = 0; //Failed
+                //200 represent status is OK
+                if (statusCode == 200) {
+                    String response = streamToString(urlConnection.getInputStream());
+                    if(i == 0) {
+                        numberOfPages = getNumberOfPages(response);
+                    }
+                    parseResult(response);
+                    result = 1; //Successful
+                } else {
+                    result = 0; //Failed
+                }
+            } catch (MalformedURLException e) {
+                Log.d(TAG, "Problem creating URL");
+                e.printStackTrace();
+            } catch (Exception e) {
+                Log.d(TAG, e.getLocalizedMessage());
+                e.printStackTrace();
+            } finally {
+                urlConnection.disconnect();
             }
-        } catch(Exception e) {
-            Log.d(TAG, e.getLocalizedMessage());
-        } finally {
-            urlConnection.disconnect();
         }
         return result;
     }
@@ -94,7 +102,7 @@ public class LoadImagesAsyncTask extends AsyncTask<String, Void, Integer> {
     @Override
     protected void onPostExecute(Integer result) {
         //Download complete, let's update UI
-        if(result == 1){
+        if (result == 1) {
             mGridAdapter.setGridData(mGridData);
         } else {
             ConnectivityManager connectivityManager =
@@ -104,7 +112,7 @@ public class LoadImagesAsyncTask extends AsyncTask<String, Void, Integer> {
                 if (activeNetworkInfo == null || !activeNetworkInfo.isConnectedOrConnecting()) {
                     Toast.makeText(mContext, "Please check the status of the network.", Toast.LENGTH_SHORT).show();
                 }
-            } catch (NullPointerException e){
+            } catch (NullPointerException e) {
                 e.printStackTrace();
                 Log.e(LOG_TAG, "Can't check if network info isConnectedOrConnecting");
             }
@@ -116,7 +124,7 @@ public class LoadImagesAsyncTask extends AsyncTask<String, Void, Integer> {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
         String line;
         String result = "";
-        while((line = bufferedReader.readLine()) != null){
+        while ((line = bufferedReader.readLine()) != null) {
             result += line;
         }
 
@@ -127,28 +135,53 @@ public class LoadImagesAsyncTask extends AsyncTask<String, Void, Integer> {
 
     /**
      * Parsing the feed results and get the list
+     *
      * @param result is result String we got from InputStreamReader
      */
-    private void parseResult(String result){
-        try{
+    private void parseResult(String result) {
+        try {
             JSONObject rootJson = new JSONObject(result);
             JSONArray hits = rootJson.optJSONArray("hits");
+
             GridItem item;
-            for(int i = 0; i<hits.length();i++){
+            for (int i = 0; i < 200; i++) {
 
                 item = new GridItem();
 
-                if(hits.length() > 0){
+                if (hits.length() > 0) {
                     JSONObject image = hits.getJSONObject(i);
-                    if(image != null){
+                    if (image != null) {
                         item.setImage(image.getString("webformatURL"));
                         item.setName(image.getString("id"));
                     }
                 }
                 mGridData.add(item);
             }
-        } catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Returning number of pages to loop through
+     *
+     * @param result is result String we got from InputStreamReader
+     */
+    private int getNumberOfPages(String result) {
+        int pages = 0;
+
+        try{
+        //Finding out number of pages
+        JSONObject rootJson = new JSONObject(result);
+
+        if(rootJson.getInt("totalHits")%200 != 0) {
+            pages = (rootJson.getInt("totalHits") / 200) + 1;
+        } else {
+            pages =  rootJson.getInt("totalHits") / 200;
+        }} catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        return pages;
     }
 }
