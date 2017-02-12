469da30ffc24585cee1341685ca8f61daccfea49
AsimQasimzade <asim.qasimzade@gmail.com>
2017-02-12 14:42:05 +0300

    Fix notification trigger time issue, set it to trigger after 1 day from last use, and then repeat every day Fix bug with adView

--- app/src/main/java/us/asimgasimzade/android/neatwallpapers/NotificationReceiver.java
+++ app/src/main/java/us/asimgasimzade/android/neatwallpapers/NotificationReceiver.java
@@ -3,6 +3,10 @@ package us.asimgasimzade.android.neatwallpapers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import us.asimgasimzade.android.neatwallpapers.Tasks.AddNotificationTask;
 
@@ -16,5 +20,4 @@ public class  NotificationReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         new AddNotificationTask(context).execute();
     }
-
 }
