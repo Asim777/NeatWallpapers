bc707fa11f0c806d11d5058cf4185d53a3301172
AsimQasimzade <asim.qasimzade@gmail.com>
2017-04-19 15:41:00 +0300

    Created Holder fragment

---
+++ app/src/main/java/us/asimgasimzade/android/neatwallpapers/SingleImageHolderFragment.java
@@ -0,0 +1,10 @@
+package us.asimgasimzade.android.neatwallpapers;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by asim on 4/19/17.
+ */
+
+public class SingleImageHolderFragment extends Fragment {
+}
